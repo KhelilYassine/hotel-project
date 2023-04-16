00f6cda6b240835bd5993c070b6e38d66142e5d4
KhelilYassine <kHELILCHERFIYASSINE@GMAIL.COM>
2023-04-16 13:32:03 +0000

    ajout de class hotel

---
+++ Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Hotel
+{
+    internal class Hotel
+    {
+        public string Nom { get; set; }
+        public string Adresse { get; set; }
+        public List<Client> Clients { get; set; }
+
+        public Hotel(string nom, string adresse)
+        {
+            Nom = nom;
+            Adresse = adresse;
+            Clients = new List<Client>();
+        }
+
+        public void AjouterClient(Client client)
+        {
+            Clients.Add(client);
+        }
+    }
+
+
+}
